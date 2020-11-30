1626873fb71669c9e8b3f4289405afa0aa6b511d
marcelacoronel <marcelacoronelp@gmail.com>
2020-11-30 10:10:25 -0500

    Maquetación y estilos barra de navegación

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logoBQNav from '../img/logoBQNav.png';
-import iconoMesas from '../img/iconoTomarPedido.png';
+import iconLogout from '../img/iconLogout.png';
+import loginNameLogo from '../img/imgNameLogo.png';
+import imgLoginBurger from '../img/imgLoginBurger.png';
+
+
+import NavBarData from '../data/NavBarData';
 
 const NavBar = () => {
-        return (
-            <div className = "navBar-color">
-                    <div>
-                        <Link to="/">
-                            <img src ={logoBQNav} alt=""/>
-                            <span>Cerrar Sesión</span>
-                        </Link>
-                        <Link to="/mesas">
-                            <img src ={iconoMesas} alt=""/>
-                            <span>Tomar Pedidos</span>
-                        </Link>
-                    </div>
+    let getDate = new Date()
+    let currentDate = getDate.toDateString();
+    let localTime = getDate.toLocaleTimeString();
+   
+    return (
+        <div className = "navBar-container">
+
+            <div className = "imgBurger">
+                <img src ={imgLoginBurger} alt=""/>
             </div>
-        )
-    }
+            <div className = "imgNameLogo">
+                <img src ={loginNameLogo} alt=""/>
+            </div>
+            <nav>
+                <ul>
+                {NavBarData.map((item, index) => {
+                    return (
+                        <li key={index} className ={item.cName} >
+                            <Link to={item.path}>
+                                <div><img src ={item.icon} alt=""/> </div>
+                                <div><span>{item.title}</span></div>
+                            </Link>
+                        </li> 
+                    )
+                })}
+                    
+                    <li>
+                        <Link to="/">
+                            <div><img src ={iconLogout} alt=""/></div>
+                            <div><span>Cerrar Sesión</span></div>
+                        </Link>
+                    </li>    
+                </ul>  
+            </nav> 
+            
+            <div className="time">
+                <span>{localTime}</span> 
+            </div>  
+            <div className="date">
+                <span>{currentDate}</span>
+            </div>                
+        </div>
+    )
+}
 
 
 export default NavBar;
