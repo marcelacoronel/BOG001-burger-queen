b9be74eb0aa4a0ad367792be692a59736f6dee21
marcelacoronel <marcelacoronelp@gmail.com>
2020-11-30 12:57:05 -0500

    funcionalidad detalle del pedido

---
+++ src/components/RowOrder.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import iconAdd from '../img/icon-add.png'
+import iconSubtract from '../img/icon-subtract.png'
+import iconRemove from '../img/icon-remove.png'
+
+
+function RowOrder(props) {
+    console.log(props);
+
+    let bill = props.bill;
+    let setBill = props.setBill;
+
+    const [number, setNumber] = useState(1);
+
+    
+    const addItem = (orderA, i) =>{
+        const orderCopy = [...orderA];
+        orderCopy[i].number += 1;
+        return orderCopy;
+
+    }
+
+    
+    const subtractItem = (orderA, i) =>{
+        const orderCopy = [...orderA];
+        orderCopy[i].number -= 1;
+        return orderCopy;
+
+    }
+
+
+    return (
+        <div className="container-rowOrder">
+            <p>{props.product.name}</p>
+            <img src={iconSubtract}  onClick={() => {(props.product.number - 1 === 0) ? props.setOrder(props.order.filter((item) => item.id !== props.index )) : props.setOrder(subtractItem(props.order, props.index))}}></img>
+            <p>{props.product.number}</p>
+            <img src={iconAdd} onClick={()=>props.setOrder(addItem(props.order, props.index)) }></img>
+            <p>{props.product.price}</p>
+            <img src={iconRemove} onClick={()=>props.setOrder(props.order.filter((item) => item.id !== props.product.id ))}></img>
+            
+        </div>
+    )
+}
+
+export default RowOrder;
+
